062d1268d0e790068e8f1ae71f2db6a690479696
Jaya Sayal <99303943+jayaMSM@users.noreply.github.com>
2023-04-19 15:47:06 -0600

    commit

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ function show2(){
 }
 
 function Calculate(){
-   const e=2.71828;    let a,b,c,d,p1, p2, p3, p4;
+   const e=2.71828;    
+   let a,b,c,d,p1, p2, p3, p4;
    let x=document.getElementById("at");
    let y=document.getElementById("rh");
    let z=document.getElementById("ws");
@@ -27,38 +28,35 @@ function Calculate(){
    let x_3=Number(z.value); // WIND SPEED
 
 
-       if ( x_1 && x_2 ){
-               a=5.8;
-               b=0.02;
-               c=(-7.9);
-               d=0;
-               p1= 1/(1+e^(-(a+b*x_1+c*x_2+d*x_3)));
+        if (x_1 && x_2) {
+                a = 5.8;
+                b = 0.02;
+                c = -7.9;
+                d = 0;
+                p1 = 1 / ((1 + e) ^ -(a + b * x_1 + c * x_2 + d * x_3));
                document.getElementById("result1").innerHTML =p1;                        
-       }
-       else if( x_2 && x_3 ){
-               a=4.5;
-               b=1.0;
-               c=(-6.4);
-               d=(-6.8);
-               p2= 1/(1+e^(-(a+b*x_1+c*x_2+d*x_3))); 
+       }else if (x_2 && x_3) {
+                a = 4.5;
+                b = 1.0;
+                c = -6.4;
+                d = -6.8;
+                p2 = 1 / ((1 + e) ^ -(a + b * x_1 + c * x_2 + d * x_3));
                document.getElementById("result2").innerHTML =p2;                        
 
-       }
-       else if( x_1 && x_3 ){
-               a=0.7;
-               b=2.2;
-               c=(-0.02);
-               d=(-6.8);
-               p3= 1/(1+e^(-(a+b*x_1+c*x_2+d*x_3)));
+       }else if (x_1 && x_3) {
+                a = 0.7;
+                b = 2.2;
+                c = -0.02;
+                d = -6.8;
+                p3 = 1 / ((1 + e) ^ -(a + b * x_1 + c * x_2 + d * x_3));
                document.getElementById("result3").innerHTML =p3;                        
 
-       }
-       else if( x_1 && x_2 && x_3 ){ 
-               a=4.6;
-               b=0.96;
-               c=0.02;
-               d=(-6.8);
-               p4= 1/(1+e^(-(a+b*x_1+c*x_2+d*x_3)));
+       }else if (x_1 && x_2 && x_3) {
+                a = 4.6;
+                b = 0.96;
+                c = 0.02;
+                d = -6.8;
+                p4 = 1 / ((1 + e) ^ -(a + b * x_1 + c * x_2 + d * x_3));
                document.getElementById("result4").innerHTML =p4;                        
 
                
